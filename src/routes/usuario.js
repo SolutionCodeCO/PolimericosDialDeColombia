e934c76e7250eb32448e702edefe92931036b433
JhoannS <sebastianzamudio2004@gmail.com>
2024-05-30 14:37:23 -0500

    todo renderizado y funcional, solo falta el back, MAY 30 2:40 PM

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -6,4 +6,11 @@ router.get('/usuario', (req, res)=>{
     res.render('user/usuario')
 })
 
+router.get('/usuario/testimonio-user', (req, res)=>{
+    res.render('user/testimonio-user')
+})
+
+router.get('/usuario/contacto-user', (req, res)=>{
+    res.render('user/contacto-user')
+})
 module.exports = router
